dea9993db51130e02a58ee49a01caee2bd9ab608
Austin Ellis <Austin@Janets-iMac.local>
2020-10-14 17:27:08 -0400

    added Current Forecast card, added 5-Day Forecast card, included moment.js as well as fontawesome and bootstrap, var for apikey and search button, forloop for placing the data onto index, added searchButton function for the click event, variable for current weather and 5 day forcest, and if/else statement to get weather data and to append inside container.

---
+++ script.js
@@ -0,0 +1,60 @@
+var searchButton = $(".searchButton");
+
+var apiKey = "7db286dbdb585f688056d0442798f334";
+
+// Forloop for persisting the data onto HMTL page
+for (var i = 0; i < localStorage.length; i++) {
+  var city = localStorage.getItem(i);
+  // console.log(localStorage.getItem("City"));
+  var cityName = $(".list-group").addClass("list-group-item");
+
+  cityName.append("<li>" + city + "</li>");
+}
+// Key count for local storage
+var keyCount = 0;
+// Search button click event
+searchButton.click(function () {
+  var searchInput = $(".searchInput").val();
+
+  // Variable for current weather working
+  var urlCurrent =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchInput +
+    "&Appid=" +
+    apiKey +
+    "&units=imperial";
+  // Variable for 5 day forecast working
+  var urlFiveDay =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    searchInput +
+    "&Appid=" +
+    apiKey +
+    "&units=imperial";
+
+  if (searchInput == "") {
+    console.log(searchInput);
+  } else {
+    $.ajax({
+      url: urlCurrent,
+      method: "GET",
+    }).then(function (response) {
+      // list-group append an li to it with just set text
+      // console.log(response.name);
+      var cityName = $(".list-group").addClass("list-group-item");
+      cityName.append("<li>" + response.name + "</li>");
+      // Local storage
+      var local = localStorage.setItem(keyCount, response.name);
+      keyCount = keyCount + 1;
+
+      // Start Current Weather append
+      var currentCard = $(".currentCard").append("<div>").addClass("card-body");
+      currentCard.empty();
+      var currentName = currentCard.append("<p>");
+      // .addClass("card-text");
+      currentCard.append(currentName);
+
+    
+    });
+
+  }
+});
